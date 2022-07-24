36e956c15ed60fa1702b0f7856150640da908af6
여찬규 <wry5533@gmail.com>
2022-07-24 18:16:47 +0900

    feat: 회원가입 데모 formik 벨리데이션 추가

--- src/pages/FormOne.tsx
+++ src/pages/FormOne.tsx
@@ -1,25 +1,109 @@
+import { Formik } from 'formik'
 import AppLayout from '@components/AppLayout'
 import Button from '@components/Button'
 import Form from '@components/Form'
 import Input from '@components/Input'
+import { ErrorMsg } from '@components/Form.styled'
 
 const labelData = [
-	{ key: 'name', name: '이름' },
+	{ key: 'email', name: '이메일' },
 	{ key: 'nickname', name: '닉네임' },
 	{ key: 'password', name: '비밀번호' },
 	{ key: 'passwordCheck', name: '비밀번호 확인' }
 ] as const
 
+const initialValues = {
+	email: '',
+	nickname: '',
+	password: '',
+	passwordCheck: ''
+}
+
+const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
 const FormOne = () => {
+	const onSubmit = (data: typeof initialValues) => {
+		console.log(data.email)
+		console.log(data.nickname)
+		console.log(data.password)
+		console.log(data.passwordCheck)
+	}
+
 	return (
 		<AppLayout title="회원가입 데모">
-			<Form>
-				<Input label={labelData[0]} />
-				<Input label={labelData[1]} />
-				<Input label={labelData[2]} />
-				<Input label={labelData[3]} />
-				<Button>회원가입</Button>
-			</Form>
+			<Formik
+				initialValues={initialValues}
+				validate={values => {
+					const errors: any = {}
+
+					if (!values.email) errors.email = '이메일은 필수입니다!'
+					else if (!emailRegExp.test(values.email))
+						errors.email = '이메일 형식이 아닙니다!'
+					if (!values.nickname) errors.nickname = '닉네임은 필수입니다!'
+					if (!values.password) errors.password = '비밀번호는 필수입니다!'
+					if (!values.passwordCheck)
+						errors.passwordCheck = '비밀번호확인은 필수입니다!'
+
+					return errors
+				}}
+				onSubmit={onSubmit}
+			>
+				{({
+					values,
+					errors,
+					// touched,
+					handleChange,
+					// handleBlur,
+					handleSubmit
+					// isSubmitting
+				}) => (
+					<Form onSubmit={handleSubmit}>
+						<div>
+							<Input
+								type="email"
+								name="email"
+								value={values.email}
+								onChange={handleChange}
+								label={labelData[0]}
+							/>
+							{errors.email && <ErrorMsg>{errors.email}</ErrorMsg>}
+						</div>
+						<div>
+							<Input
+								type="text"
+								name="nickname"
+								value={values.nickname}
+								onChange={handleChange}
+								label={labelData[1]}
+							/>
+							{errors.nickname && <ErrorMsg>{errors.nickname}</ErrorMsg>}
+						</div>
+						<div>
+							<Input
+								type="password"
+								name="password"
+								value={values.password}
+								onChange={handleChange}
+								label={labelData[2]}
+							/>
+							{errors.password && <ErrorMsg>{errors.password}</ErrorMsg>}
+						</div>
+						<div>
+							<Input
+								type="password"
+								name="passwordCheck"
+								value={values.passwordCheck}
+								onChange={handleChange}
+								label={labelData[3]}
+							/>
+							{errors.passwordCheck && (
+								<ErrorMsg>{errors.passwordCheck}</ErrorMsg>
+							)}
+						</div>
+						<Button type="submit">회원가입</Button>
+					</Form>
+				)}
+			</Formik>
 		</AppLayout>
 	)
 }
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,7 @@
 export default {
 	primary: '#16a085',
 	secondary: '#1abc9c',
+	danger: '#e74c3c',
 	textColor: '#2f3640',
 	descriptColor: '#565656',
 	bgColor: '#f5f6fa',
