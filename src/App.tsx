832134488e5d549d0142953e97b6c2c143eb8e0f
여찬규 <cgyeo5533@gmail.com>
2022-09-18 17:17:22 +0900

    feat: Update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import Spinner from '@components/ui/Spinner'
 import Header from '@components/Header'
 import GlobalStyle from '@styles/GlobalStyle'
 import RouterConfig from './RouterConfig'
+import '@with-me/style/dist/global.css'
+import { Button } from '@with-me/ui'
 
 const App = () => {
 	return (
 		<BrowserRouter>
+			<Button>test</Button>
 			<GlobalStyle />
 			<Header />
 			<Suspense fallback={<Spinner />}>
